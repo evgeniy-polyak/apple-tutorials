9813254ec42ea9d786788e6ec3e6f40a1059a25d
epolyak <polyak.ev@gazprom-neft.ru>
2026-06-03 10:47:44 +0300

    Initial commit

---
+++ AppleTutorials/AppleTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialsApp.swift
+//  AppleTutorials
+//
+//  Created by Evgeniy Polyak on 03.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
