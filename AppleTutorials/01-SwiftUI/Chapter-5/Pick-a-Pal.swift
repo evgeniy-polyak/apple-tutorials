a61a31c7ba3c0cc3e710b3165465191f4f5fe6af
epolyak <polyak.ev@gazprom-neft.ru>
2026-06-08 13:23:01 +0300

    Chapter 5

--- AppleTutorials/01-SwiftUI/Chapter-5/Pick-a-Pal.swift
+++ AppleTutorials/01-SwiftUI/Chapter-5/Pick-a-Pal.swift
@@ -6,25 +6,54 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct Person: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
 
 struct Pick_a_Pal: View {
-    @State private var nameList: [String] = [
-        "Евгений", "Жанна", "Михаил", "Ксения",
+    @State private var personList: [Person] = [
+        
     ]
     
     @State private var newName: String = ""
-    @State private var pickedName = ""
-//    @State private var schould
+    @State private var pickedName = " "
+    @State private var schouldRemovePicked = false
+    @State private var selectedID: Person.ID?
     
     var body: some View {
         VStack {
-            Text(pickedName.isEmpty ? "" : pickedName)
-            
-            List {
-                ForEach(nameList, id: \.description) { name in
-                    Text(name)
-                }
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
             }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? "" : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
+            
+            List(personList, selection: $selectedID) { name in
+                Text(name.name)
+                    .listRowBackground(name.id == selectedID ? Color.secondary.opacity(0.15) : nil)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .onChange(of: selectedID, { oldValue, newValue in
+                if oldValue != newValue {
+                    pickedName = personList.first(where: {$0.id == newValue})!.name
+                }
+            })
+            .navigationTitle("Список Имен")
 //            .padding()
             
             TextField("Add name:", text: $newName)
@@ -32,20 +61,35 @@ struct Pick_a_Pal: View {
                 .onSubmit {
                     withAnimation {
                         if !newName.isEmpty {
-                            nameList.append(newName)
+                            personList.append(Person(newName))
                             newName = ""
                         }
                     }
                 }
             
             Divider()
-            Button("Pick Random Name") {
-                if let randomName = nameList.randomElement() {
+            
+            Toggle("Remove when picked", isOn: $schouldRemovePicked)
+            
+            Button {
+                if let randomName = personList.randomElement()?.name {
                     pickedName = randomName
+                    if schouldRemovePicked {
+                        personList.removeAll { name in
+                            return name.name == pickedName
+                        }
+                        pickedName = " "
+                    }
                 } else {
                     pickedName = ""
                 }
+            } label: {
+                Text("Pick Random Element")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
             
         }
         .padding()
